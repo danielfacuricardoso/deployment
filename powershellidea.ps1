6fc19c9f4f011d88387e4e05d590b3976a8dbd2a
Daniel Facuri <danieus_@hotmail.com>
2018-01-29 11:09:49 -0200

    Update powershellidea.ps1

--- powershellidea.ps1
+++ powershellidea.ps1
@@ -1,2 +1,3 @@
 #Filter a Software if it installed or Not
-gwmi win32_product -filter "Name LIKE '%NameOfSoftware%'" | Select Name
+$name = gwmi win32_product -filter "Name LIKE '%NameOfSoftware%'" | Select Name
+#Next step - Save Output of variable $name
