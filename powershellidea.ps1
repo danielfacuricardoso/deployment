790233fa1e9e4da987b2ae71dcc06ec21e331dcd
Daniel Facuri <danieus_@hotmail.com>
2018-01-29 12:22:59 -0200

    Update powershellidea.ps1

--- powershellidea.ps1
+++ powershellidea.ps1
@@ -1,5 +1,32 @@
 #1st Step - Filter a Software if it installed or Not
 $name = gwmi win32_product -filter "Name LIKE '%NameOfSoftware%'" | Select Name
 #2nd step - Save Output of variable $name
+
 #3th Step - Apply conditional action acording of Output of variable $name
 #4th step - install or not install app
+
+-----
+second possibility:https://social.technet.microsoft.com/Forums/azure/en-US/8c2c8111-8995-4ff6-a25c-e0f48136e14f/determine-if-service-exists?forum=winserverpowershell
+codigo:
+$serviceName = 'NameOfService'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+
+    If ((Get-Service $serviceName).Status -eq 'Running') {
+
+       Write-Host "$serviceName found, but it is running."
+    #    Stop-Service $serviceName
+    #    Write-Host "Stopping $serviceName"
+
+    } Else {
+
+        Write-Host "$serviceName found, but it is not running." 
+
+    }
+
+} Else {
+
+    Write-Host "$serviceName not found" <---- replace for install code
+
+}
+
