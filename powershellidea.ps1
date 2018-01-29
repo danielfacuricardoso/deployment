c5e25d36bc4dff0c7865a9c3bbbe2fa9813131a4
Daniel Facuri <danieus_@hotmail.com>
2018-01-29 11:18:41 -0200

    Update powershellidea.ps1

--- powershellidea.ps1
+++ powershellidea.ps1
@@ -1,3 +1,5 @@
 #Filter a Software if it installed or Not
 $name = gwmi win32_product -filter "Name LIKE '%NameOfSoftware%'" | Select Name
 #Next step - Save Output of variable $name
+#3th Step - Apply conditional action acording of Output of variable $name
+#4th step - install or not install app
